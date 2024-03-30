967d22d9f47958249709ecf2f0134f458d4be1ff
Abhijit_garedi <154952366+Abhijit-garedi@users.noreply.github.com>
2024-03-30 04:54:11 +0000

    variable.js

---
+++ Basics/variable.js
@@ -0,0 +1,16 @@
+const accountId = 144553
+let accountEmail = "abhijit@gmail.com"
+var accountPassword = "12345"
+accountCity = "Janakpur"
+
+//accountId = 2
+
+accountEmail = "havic.com"
+accountPassword = "21212121"
+accountCity = "sonagachi"
+
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity]);
+
+ 
