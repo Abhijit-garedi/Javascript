d4157a47b1e848fbc3fd8284922f0913df182b9a
Abhijit_garedi <154952366+Abhijit-garedi@users.noreply.github.com>
2024-03-30 04:50:21 +0000

    02_dataTypes.js

---
+++ Basics/02_dataTypes.js
@@ -0,0 +1,36 @@
+"use Strict"; //treat all js code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+console.log(3 + 3);  
+
+// code readability increased while changing lines 
+
+console.log("hitesh")
+
+
+let name = "abhijit"
+
+let age = 20
+
+let isLoggedIn = false
+let state = null
+
+//number => 2 to power 53
+
+//bright
+
+//string => ""
+
+//boolean => ture/false
+
+//null => standalone value
+
+//underdefine =>
+
+// symbol 
+
+//Object
+
+console.log(typeof "abhijit")
+console.log(typeof undefined)
