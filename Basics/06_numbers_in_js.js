76310df7b42deb20c844cabaa88205430870338a
Abhijit-garedi <paljustwin@gmail.com>
2024-04-14 22:11:59 +0530

    number in js

---
+++ Basics/06_numbers_in_js.js
@@ -0,0 +1,23 @@
+const score = 400
+
+const balance = new Number(100)
+console.log(balance);
+
+ console.log(balance.toString().length);
+
+
+ console.log(balance.toFixed(2));// specially used in E-commrc web for too fixed price value like
+ // after calculation( gst , discount , dilivery etc) price can me Rs.251.6678 which will be very 
+ // unprofessional thats why it is used ( tofixed (2) means 251.66)
+
+ const otherNumber = 123.8966
+
+ console.log(otherNumber.toPrecision(3));
+
+ const hundreds = 1000000
+ console.log(hundreds.toLocaleString('en-IN'));
+
+ 
+
+
+
