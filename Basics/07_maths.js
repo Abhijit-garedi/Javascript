e5d4474a088f8857c39cc31dbbfae1327ad55aed
Abhijit-garedi <paljustwin@gmail.com>
2024-04-18 11:22:15 +0530

    math libarary

--- Basics/07_maths.js
+++ Basics/07_maths.js
@@ -1,3 +1,24 @@
 // Math libarary
 
  console.log(Math);
+   
+ console.log(Math.abs(-4)); // by using this minus value becomes +value
+ // positive remains positive only negative becomes negative
+
+console.log(Math.round(4.6)); //round of like 4.6 = 5 
+
+console.log(Math.ceil(4.9)); // top value like 4.9 = 5
+//jara se bhi 4 se upper gaya to 5 hi lega 
+
+console.log(Math.floor(4.6)); // bottom value or lowest value
+// a nicche ka value hi lega like 4.6 = 4 
+
+
+console.log(Math.min(4, 3, 6, 9));
+
+console.log(Math.max(4, 3, 6, 8));
+
+console.log(Math.random()); // mostly comes in decimal 
+
+
+console.log(Math.random()*10 + 1); 
