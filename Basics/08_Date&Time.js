3f0b3cbd0abff9db18a29f6b94c2f5848be5cba8
Abhijit-garedi <paljustwin@gmail.com>
2024-05-23 14:10:15 +0530

    Date & Time

--- Basics/08_Date&Time.js
+++ Basics/08_Date&Time.js
@@ -1,2 +1,9 @@
 // data and time in javascript
 
+let myDate = new Date()
+console.log(myDate.toString());
+console.log(myDate.toLocaleDateString());
+console.log(myDate.toLocalsString());
+console.log(typeof myDate);
+
+let myCreateDate = new Date(2023, 0, 23)
