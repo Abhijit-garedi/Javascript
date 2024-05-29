a5ac81ccc9774789df0b4682ab68a380147e3b3c
Abhijit-garedi <paljustwin@gmail.com>
2024-05-29 23:53:31 +0530

    checking voting eligiblity

---
+++ Basic Program/Voting_Eligibility.js
@@ -0,0 +1,11 @@
+// in this program we will find out whether the individual is 
+// eligible to vote or not by his age
+
+
+let age = 20;
+
+if (age >= 18) {
+    console.log("You are eligible to vote.");
+} else {
+    console.log("You are not eligible to vote.");
+}
