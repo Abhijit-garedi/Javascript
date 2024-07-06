81070c5415dced7e04319faae3ffcfc47445fbea
Abhijit-garedi <paljustwin@gmail.com>
2024-07-06 23:42:47 +0530

    largest among two numbers

---
+++ Loops/Largest_among_two_numbers.js
@@ -0,0 +1,13 @@
+// Program for finding the largest of two numbers
+
+
+let a = 10;
+let b = 20;
+
+if (a > b) {
+    console.log(a + " is larger than " + b);
+} else if (a < b) {
+    console.log(b + " is larger than " + a);
+} else {
+    console.log(a + " and " + b + " are equal.");
+}
