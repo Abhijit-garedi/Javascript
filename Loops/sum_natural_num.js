c6cb3f380529381379277c349f5d3d70a5c13340
Abhijit-garedi <paljustwin@gmail.com>
2024-07-06 23:46:06 +0530

    sum of first five natural numbers using for loop

---
+++ Loops/sum_natural_num.js
@@ -0,0 +1,9 @@
+// Program for sum of first 5 naturals numbers using "for" loop
+
+  let sum = 0;
+
+   for (let i = 1; i <= 5; i++) {
+      sum += i;
+    }
+
+      console.log("Sum of first 5 natural numbers is " + sum);
