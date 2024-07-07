cb33a29688a85dd582bfd5899257686517d05307
Abhijit-garedi <paljustwin@gmail.com>
2024-07-07 23:50:19 +0530

    iterates over an array and prints each element

---
+++ Arrays/iterates.js
@@ -0,0 +1,8 @@
+  
+
+     // This program iterates over an array and prints each element
+
+     const animals = ["Cat", "Dog", "Elephant"];
+for (let i = 0; i < animals.length; i++) {
+    console.log(animals[i]);
+}
