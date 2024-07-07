54f6e7f1c881e1bf4687362fb262917507fdf24e
Abhijit-garedi <paljustwin@gmail.com>
2024-07-07 23:52:08 +0530

    index of an element in an array

---
+++ Arrays/elements_in_array.js
@@ -0,0 +1,12 @@
+    
+
+     // this program finds the index of an element in an array using "indexOf"
+
+     const colors = ["Red", "Green", "Blue"];
+     const index = colors.indexOf("Green");
+     if (index !== -1) {
+         console.log("Green found at index: " + index);
+     } else {
+         console.log("Green not found");
+     }
+     
