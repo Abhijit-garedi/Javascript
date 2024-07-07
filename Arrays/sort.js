ea8a6fcdbed8e2fed3b533110adfb066f008b72b
Abhijit-garedi <paljustwin@gmail.com>
2024-07-07 23:54:50 +0530

    sorting array

---
+++ Arrays/sort.js
@@ -0,0 +1,9 @@
+
+
+
+     // this program sorts an array of numbers in ascending order usng the "sort" method
+
+     const numbers = [5, 3, 8, 1, 2];
+     numbers.sort((a, b) => a - b); // Sorts the array in ascending order
+     console.log(numbers);
+     
