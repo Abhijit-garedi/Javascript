b7b7a83f1fd50a7b8701ef09e0b0182f4bcb14da
Abhijit-garedi <paljustwin@gmail.com>
2024-07-09 21:04:09 +0530

    Find Even no. from Array

---
+++ Arrays/Array Basic/find_even_no_from_Array.js
@@ -0,0 +1,8 @@
+  
+
+   // JS program which will filter Even Numbers from Array
+
+   let arr = [1, 2, 3, 4, 5, 6, 7, 8];
+   let evenNumbers = arr.filter(num => num % 2 === 0);
+   console.log(evenNumbers); 
+   
