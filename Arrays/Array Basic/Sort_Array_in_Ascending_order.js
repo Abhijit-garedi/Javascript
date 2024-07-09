0ebcd28772b931d233cef755ddefb1aad6e8fb66
Abhijit-garedi <paljustwin@gmail.com>
2024-07-09 21:07:19 +0530

    sorting an array into ascending order

---
+++ Arrays/Array Basic/Sort_Array_in_Ascending_order.js
@@ -0,0 +1,10 @@
+ 
+  // JS program which will sort an array ascending order
+
+  let arr = [3, 1, 4, 1, 5, 9];
+  let sortedArr = arr.sort((a, b) => a - b);
+  console.log(sortedArr); 
+  
+  
+    // Output: [1, 1, 3, 4, 5, 9]
+  
