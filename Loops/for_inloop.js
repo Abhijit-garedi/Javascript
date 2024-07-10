fde2c0b1dabec63b704ba1603bc99f106485688b
Abhijit-garedi <paljustwin@gmail.com>
2024-07-10 23:58:57 +0530

    for in loop

---
+++ Loops/for_inloop.js
@@ -0,0 +1,7 @@
+
+
+   // Iterate over the properties of an object
+const person = {name: 'John', age: 30, city: 'New York'};
+for (let key in person) {
+    console.log(key + ": " + person[key]);
+}
