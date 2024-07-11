1771be2b9d8743f9b25ad4b2e3e459422f0c912d
Abhijit-garedi <paljustwin@gmail.com>
2024-07-11 23:12:17 +0530

    factorial of a number

---
+++ Loops/Medium level programs/factorial_of_a_number.js
@@ -0,0 +1,16 @@
+
+
+  // JS program to find the factorial of a number 
+
+  function factorial(n) {
+    let result = 1;
+    for (let i = 1; i <= n; i++) {
+        result *= i;
+    }
+    return result;
+  }
+   console.log(factorial(5)); 
+
+
+   // factorial of 5 = 120
+   
