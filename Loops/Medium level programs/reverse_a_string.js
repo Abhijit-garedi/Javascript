8fa4f8e0476a7eb3189b16cea3c2a7892561856d
Abhijit-garedi <paljustwin@gmail.com>
2024-07-11 23:18:43 +0530

    reverse a string

---
+++ Loops/Medium level programs/reverse_a_string.js
@@ -0,0 +1,11 @@
+
+
+ // JS program to reverse a string
+ function reverseString(str) {
+    let reversed = '';
+    for (let i = str.length - 1; i >= 0; i--) {
+        reversed += str[i];
+    }
+    return reversed;
+}
+console.log(reverseString('hello')); 
