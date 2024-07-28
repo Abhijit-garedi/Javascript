f003a5c809e0b598dfe5792d656432db3315a160
Abhijit-garedi <paljustwin@gmail.com>
2024-07-28 22:42:11 +0530

    array basic

---
+++ Basic part 2/01_arrays.js
@@ -0,0 +1,26 @@
+
+// Array
+
+
+ const myArr = [1, 2, 3, 4]
+
+ // shallow copy = same reference 
+   
+ const myHeros = ["shaktiman", "nagraj"]
+
+ const myArr2 = new Array(1, 2, 3, 4, )
+ console.log(myArr[1]);
+
+
+
+     // array method
+     //myArr.push(6)
+     //myArr.push(7)
+     //myArr.pop()
+
+    //myArr.unshift(9)
+
+    console.log(myArr.includes(9));
+    console.log(myArr.indexOf(3));
+    
+     console.log(myArr);
