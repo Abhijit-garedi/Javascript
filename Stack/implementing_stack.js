6a252ae72478a448091def84f03e1491ed897036
Abhijit-garedi <paljustwin@gmail.com>
2024-12-01 16:12:10 +0530

    dublicates

---
+++ Stack/implementing_stack.js
@@ -0,0 +1,46 @@
+// To implement stack
+
+class Stack {
+    constructor() {
+      this.items = [];
+    }
+  
+    push(element)
+    {
+      this.items.push(element); 
+    }
+  
+    pop()
+    {
+      return this.items.pop(); 
+    }
+  
+    peek()
+    {
+      return this.items[this.items.length - 1]; 
+    }
+  
+    isEmpty()
+    {
+      return this.items.length === 0;
+    }
+  
+    printStack() 
+    {
+      console.log(this.items.join(" -> ")); 
+    }
+  }
+
+  
+  
+  const myStack = new Stack();
+  myStack.push(10);
+  myStack.push(20);
+  myStack.printStack();
+  console.log(myStack.pop()); 
+  console.log(myStack.peek()); // peek return top element without removing it
+
+  // output 10 -> 20
+//20
+//10
+  
