bc08d8249ee5de3415d7bdb7d8b863344497e07c
Abhijit-garedi <paljustwin@gmail.com>
2024-12-01 16:17:05 +0530

    if else program

---
+++ vscode/if_else.js
@@ -0,0 +1,18 @@
+// basic js program using if else 
+
+// This program checks if a number is positive, negative, or zero
+
+let number = 5;
+
+if (number > 0) 
+    {
+    console.log("The number is positive.");
+} 
+else if (number < 0) 
+    {
+    console.log("The number is negative.");
+} 
+else 
+{
+    console.log("The number is zero.");
+}
